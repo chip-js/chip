chip-js/chip

commit a8bbc80725424fbeb5a6c5c96558a124019d7684

Fixed bug in compare with NaN. Fixed tests. Added ability for select to choose objects and not just values.

dist/chip.js
@@ -2088,11 +2088,29 @@ if (!Date.prototype.toISOString) {
   });
 
   chip.binding('value', function(element, expr, controller) {
-    var events, getValue, observer, setValue;
+    var events, fieldExpr, getValue, observer, prefix, selectValueField, setValue, watchExpr;
+    prefix = controller.app.bindingPrefix;
+    watchExpr = expr;
+    if (element.is('select')) {
+      fieldExpr = element.attr(prefix + 'value-field');
+      element.removeAttr(prefix + 'value-field');
+      selectValueField = fieldExpr ? controller["eval"](fieldExpr) : null;
+      chip.lastSelectValueField = selectValueField;
+    }
+    if (element.is('option') && chip.lastSelectValueField) {
+      selectValueField = chip.lastSelectValueField;
+      watchExpr += '.' + selectValueField;
+    }
     getValue = element.attr('type') === 'checkbox' ? function() {
       return element.prop('checked');
     } : element.is(':not(input,select,textarea,option)') ? function() {
       return element.find('input:radio:checked').val();
+    } : selectValueField && element.is('select') ? function(realValue) {
+      if (realValue) {
+        return $(element.get(0).options[element.get(0).selectedIndex]).data('value');
+      } else {
+        return element.val();
+      }
     } : function() {
       return element.val();
     };
@@ -2102,11 +2120,14 @@ if (!Date.prototype.toISOString) {
       element.find('input:radio:checked').prop('checked', false);
       return element.find('input:radio[value="' + value + '"]').prop('checked', true);
     } : function(value) {
-      return element.val(value);
+      element.val(selectValueField && value[selectValueField] || value);
+      if (selectValueField) {
+        return element.data('value', value);
+      }
     };
-    observer = controller.watch(expr, function(value) {
+    observer = controller.watch(watchExpr, function(value) {
       if (getValue() != value) {
-        return setValue(value);
+        return setValue(controller["eval"](expr));
       }
     });
     if (element.is('option')) {
@@ -2115,13 +2136,13 @@ if (!Date.prototype.toISOString) {
     if (element.is('select')) {
       setTimeout(function() {
         setValue(controller["eval"](expr));
-        return controller.evalSetter(expr, getValue());
+        return controller.evalSetter(expr, getValue(true));
       });
     } else {
       controller.evalSetter(expr, getValue());
     }
-    events = element.attr('chip-value-events') || 'change';
-    element.removeAttr('chip-value-events');
+    events = element.attr(prefix + 'value-events') || 'change';
+    element.removeAttr(prefix + 'value-events');
     if (element.is(':text')) {
       element.on('keydown', function(event) {
         if (event.keyCode === 13) {
@@ -2131,7 +2152,7 @@ if (!Date.prototype.toISOString) {
     }
     return element.on(events, function() {
       if (getValue() !== observer.oldValue) {
-        controller.evalSetter(expr, getValue());
+        controller.evalSetter(expr, getValue(true));
         observer.skipNextSync();
         return controller.sync();
       }
@@ -2670,7 +2691,7 @@ if (!Date.prototype.toISOString) {
       } else if (value && oldValue && typeof value === 'object' && typeof oldValue === 'object') {
         valueValue = value.valueOf();
         oldValueValue = oldValue.valueOf();
-        if (valueValue !== value && oldValueValue !== oldValue) {
+        if (typeof valueValue !== 'object' && typeof oldValueValue !== 'object') {
           return valueValue !== oldValueValue;
         } else {
           changeRecords = compare.objects(value, oldValue);
@@ -2681,28 +2702,42 @@ if (!Date.prototype.toISOString) {
           }
         }
       } else {
+        return compare.basic(value, oldValue);
+      }
+    };
+    compare.basic = function(value, oldValue) {
+      var oldValueValue, valueValue;
+      if (value && oldValue && typeof value === 'object' && typeof oldValue === 'object') {
+        valueValue = value.valueOf();
+        oldValueValue = oldValue.valueOf();
+        if (typeof valueValue !== 'object' && typeof oldValueValue !== 'object') {
+          return compare.basic(valueValue, oldValueValue);
+        }
+      } else if (typeof value === 'number' && typeof oldValue === 'number' && isNaN(value) && isNaN(oldValue)) {
+        return false;
+      } else {
         return value !== oldValue;
       }
     };
     compare.objects = function(object, oldObject) {
-      var changeRecords, newValue, oldValue, prop;
+      var changeRecords, oldValue, prop, value;
       changeRecords = [];
       for (prop in oldObject) {
         oldValue = oldObject[prop];
-        newValue = object[prop];
-        if (newValue !== void 0 && newValue === oldValue) {
+        value = object[prop];
+        if (value !== void 0 && !compare.basic(value, oldValue)) {
           continue;
         }
         if (!(prop in object)) {
           changeRecords.push(newChange(object, 'deleted', prop, oldValue));
           continue;
         }
-        if (newValue !== oldValue) {
+        if (compare.basic(value, oldValue)) {
           changeRecords.push(newChange(object, 'updated', prop, oldValue));
         }
       }
       for (prop in object) {
-        newValue = object[prop];
+        value = object[prop];
         if (prop in oldObject) {
           continue;
         }
@@ -2792,7 +2827,7 @@ if (!Date.prototype.toISOString) {
     sharedPrefix = function(current, old, searchLength) {
       var i, _j;
       for (i = _j = 0; 0 <= searchLength ? _j < searchLength : _j > searchLength; i = 0 <= searchLength ? ++_j : --_j) {
-        if (current[i] !== old[i]) {
+        if (compare.basic(current[i], old[i])) {
           return i;
         }
       }
@@ -2803,7 +2838,7 @@ if (!Date.prototype.toISOString) {
       index1 = current.length;
       index2 = old.length;
       count = 0;
-      while (count < searchLength && current[--index1] === old[--index2]) {
+      while (count < searchLength && !compare.basic(current[--index1], old[--index2])) {
         count++;
       }
       return count;
@@ -2876,7 +2911,7 @@ if (!Date.prototype.toISOString) {
       }
       for (i = _l = 1; 1 <= rowCount ? _l < rowCount : _l > rowCount; i = 1 <= rowCount ? ++_l : --_l) {
         for (j = _m = 1; 1 <= columnCount ? _m < columnCount : _m > columnCount; j = 1 <= columnCount ? ++_m : --_m) {
-          if (current[currentStart + j - 1] === old[oldStart + i - 1]) {
+          if (!compare.basic(current[currentStart + j - 1], old[oldStart + i - 1])) {
             distances[i][j] = distances[i - 1][j - 1];
           } else {
             north = distances[i - 1][j] + 1;

docs/src/compare.html
@@ -155,7 +155,7 @@ returned. Otherwise  <code>true</code> will be returned.</p>
 
             </div>
             
-            <div class="content"><div class='highlight'><pre>      <span class="keyword">if</span> valueValue <span class="keyword">isnt</span> value <span class="keyword">and</span> oldValueValue <span class="keyword">isnt</span> oldValue
+            <div class="content"><div class='highlight'><pre>      <span class="keyword">if</span> <span class="keyword">typeof</span> valueValue <span class="keyword">isnt</span> <span class="string">'object'</span> <span class="keyword">and</span> <span class="keyword">typeof</span> oldValueValue <span class="keyword">isnt</span> <span class="string">'object'</span>
         <span class="keyword">return</span> valueValue <span class="keyword">isnt</span> oldValueValue
       <span class="keyword">else</span>
         changeRecords = compare.objects value, oldValue
@@ -175,7 +175,7 @@ returned. Otherwise  <code>true</code> will be returned.</p>
             </div>
             
             <div class="content"><div class='highlight'><pre>    <span class="keyword">else</span>
-      <span class="keyword">return</span> value <span class="keyword">isnt</span> oldValue</pre></div></div>
+      <span class="keyword">return</span> compare.basic value, oldValue</pre></div></div>
             
         </li>
         
@@ -185,6 +185,72 @@ returned. Otherwise  <code>true</code> will be returned.</p>
               
               <div class="pilwrap ">
                 <a class="pilcrow" href="#section-10">&#182;</a>
+              </div>
+              <p>Diffs two basic types, returning true if changed or false if not</p>
+
+            </div>
+            
+            <div class="content"><div class='highlight'><pre>  compare.<span class="function"><span class="title">basic</span></span> = (value, oldValue) -&gt;
+   <span class="keyword">if</span> value <span class="keyword">and</span> oldValue <span class="keyword">and</span> <span class="keyword">typeof</span> value <span class="keyword">is</span> <span class="string">'object'</span> <span class="keyword">and</span> <span class="keyword">typeof</span> oldValue <span class="keyword">is</span> <span class="string">'object'</span></pre></div></div>
+            
+        </li>
+        
+        
+        <li id="section-11">
+            <div class="annotation">
+              
+              <div class="pilwrap ">
+                <a class="pilcrow" href="#section-11">&#182;</a>
+              </div>
+              <p>Allow dates and Number/String objects to be compared</p>
+
+            </div>
+            
+            <div class="content"><div class='highlight'><pre>      valueValue = value.valueOf()
+      oldValueValue = oldValue.valueOf()</pre></div></div>
+            
+        </li>
+        
+        
+        <li id="section-12">
+            <div class="annotation">
+              
+              <div class="pilwrap ">
+                <a class="pilcrow" href="#section-12">&#182;</a>
+              </div>
+              <p>Allow dates and Number/String objects to be compared</p>
+
+            </div>
+            
+            <div class="content"><div class='highlight'><pre>      <span class="keyword">if</span> <span class="keyword">typeof</span> valueValue <span class="keyword">isnt</span> <span class="string">'object'</span> <span class="keyword">and</span> <span class="keyword">typeof</span> oldValueValue <span class="keyword">isnt</span> <span class="string">'object'</span>
+        <span class="keyword">return</span> compare.basic valueValue, oldValueValue</pre></div></div>
+            
+        </li>
+        
+        
+        <li id="section-13">
+            <div class="annotation">
+              
+              <div class="pilwrap ">
+                <a class="pilcrow" href="#section-13">&#182;</a>
+              </div>
+              <p>If a value has changed call the callback</p>
+
+            </div>
+            
+            <div class="content"><div class='highlight'><pre>    <span class="keyword">else</span> <span class="keyword">if</span> <span class="keyword">typeof</span> value <span class="keyword">is</span> <span class="string">'number'</span> <span class="keyword">and</span> <span class="keyword">typeof</span> oldValue <span class="keyword">is</span> <span class="string">'number'</span> <span class="keyword">and</span> isNaN(value) <span class="keyword">and</span> isNaN(oldValue)
+      <span class="keyword">return</span> <span class="literal">false</span>
+    <span class="keyword">else</span>
+      <span class="keyword">return</span> value <span class="keyword">isnt</span> oldValue</pre></div></div>
+            
+        </li>
+        
+        
+        <li id="section-14">
+            <div class="annotation">
+              
+              <div class="pilwrap ">
+                <a class="pilcrow" href="#section-14">&#182;</a>
               </div>
               <p>Diffs two objects returning an array of change records. The change record looks like:</p>
 <pre><code class="lang-javascript">{
@@ -202,42 +268,42 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         </li>
         
         
-        <li id="section-11">
+        <li id="section-15">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-11">&#182;</a>
+                <a class="pilcrow" href="#section-15">&#182;</a>
               </div>
               <p>Goes through the old object (should be a clone) and look for things that are now gone or changed</p>
 
             </div>
             
             <div class="content"><div class='highlight'><pre>    <span class="keyword">for</span> prop, oldValue <span class="keyword">of</span> oldObject
-      newValue = object[prop]</pre></div></div>
+      value = object[prop]</pre></div></div>
             
         </li>
         
         
-        <li id="section-12">
+        <li id="section-16">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-12">&#182;</a>
+                <a class="pilcrow" href="#section-16">&#182;</a>
               </div>
               <p>Allow for the case of obj.prop = undefined (which is a new property, even if it is undefined)</p>
 
             </div>
             
-            <div class="content"><div class='highlight'><pre>      <span class="keyword">continue</span> <span class="keyword">if</span> newValue <span class="keyword">isnt</span> <span class="literal">undefined</span> <span class="keyword">and</span> newValue <span class="keyword">is</span> oldValue</pre></div></div>
+            <div class="content"><div class='highlight'><pre>      <span class="keyword">continue</span> <span class="keyword">if</span> value <span class="keyword">isnt</span> <span class="literal">undefined</span> <span class="keyword">and</span> <span class="keyword">not</span> compare.basic value, oldValue</pre></div></div>
             
         </li>
         
         
-        <li id="section-13">
+        <li id="section-17">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-13">&#182;</a>
+                <a class="pilcrow" href="#section-17">&#182;</a>
               </div>
               <p>If the property is gone it was removed</p>
 
@@ -247,23 +313,23 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         changeRecords.push newChange object, <span class="string">'deleted'</span>, prop, oldValue
         <span class="keyword">continue</span>
       
-      <span class="keyword">if</span> newValue <span class="keyword">isnt</span> oldValue
+      <span class="keyword">if</span> compare.basic value, oldValue
         changeRecords.push newChange object, <span class="string">'updated'</span>, prop, oldValue</pre></div></div>
             
         </li>
         
         
-        <li id="section-14">
+        <li id="section-18">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-14">&#182;</a>
+                <a class="pilcrow" href="#section-18">&#182;</a>
               </div>
               <p>Goes through the old object and looks for things that are new</p>
 
             </div>
             
-            <div class="content"><div class='highlight'><pre>    <span class="keyword">for</span> prop, newValue <span class="keyword">of</span> object
+            <div class="content"><div class='highlight'><pre>    <span class="keyword">for</span> prop, value <span class="keyword">of</span> object
       <span class="keyword">continue</span> <span class="keyword">if</span> `<span class="javascript">prop <span class="keyword">in</span> oldObject</span>`
       
       changeRecords.push newChange object, <span class="string">'new'</span>, prop
@@ -276,11 +342,11 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         </li>
         
         
-        <li id="section-15">
+        <li id="section-19">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-15">&#182;</a>
+                <a class="pilcrow" href="#section-19">&#182;</a>
               </div>
               <p>Creates a change record for the object changes</p>
 
@@ -304,11 +370,11 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         </li>
         
         
-        <li id="section-16">
+        <li id="section-20">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-16">&#182;</a>
+                <a class="pilcrow" href="#section-20">&#182;</a>
               </div>
               <p>Diffs two arrays returning an array of splices. A splice object looks like:</p>
 <pre><code class="lang-javascript">{
@@ -340,11 +406,11 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         </li>
         
         
-        <li id="section-17">
+        <li id="section-21">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-17">&#182;</a>
+                <a class="pilcrow" href="#section-21">&#182;</a>
               </div>
               <p>if nothing was added, only removed from one spot</p>
 
@@ -356,11 +422,11 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         </li>
         
         
-        <li id="section-18">
+        <li id="section-22">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-18">&#182;</a>
+                <a class="pilcrow" href="#section-22">&#182;</a>
               </div>
               <p>if nothing was removed, only added to one spot</p>
 
@@ -372,11 +438,11 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         </li>
         
         
-        <li id="section-19">
+        <li id="section-23">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-19">&#182;</a>
+                <a class="pilcrow" href="#section-23">&#182;</a>
               </div>
               <p>a mixture of adds and removes</p>
 
@@ -423,11 +489,11 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         </li>
         
         
-        <li id="section-20">
+        <li id="section-24">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-20">&#182;</a>
+                <a class="pilcrow" href="#section-24">&#182;</a>
               </div>
               <p>find the number of items at the beginning that are the same</p>
 
@@ -435,17 +501,17 @@ returned. Otherwise  <code>true</code> will be returned.</p>
             
             <div class="content"><div class='highlight'><pre>  <span class="function"><span class="title">sharedPrefix</span></span> = (current, old, searchLength) -&gt;
     <span class="keyword">for</span> i <span class="keyword">in</span> [<span class="number">0.</span>..searchLength]
-      <span class="keyword">return</span> i <span class="keyword">unless</span> current[i] <span class="keyword">is</span> old[i]
+      <span class="keyword">return</span> i <span class="keyword">if</span> compare.basic current[i], old[i]
     <span class="keyword">return</span> searchLength</pre></div></div>
             
         </li>
         
         
-        <li id="section-21">
+        <li id="section-25">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-21">&#182;</a>
+                <a class="pilcrow" href="#section-25">&#182;</a>
               </div>
               <p>find the number of items at the end that are the same</p>
 
@@ -455,7 +521,7 @@ returned. Otherwise  <code>true</code> will be returned.</p>
     index1 = current.length
     index2 = old.length
     count = <span class="number">0</span>
-    <span class="keyword">while</span> count &lt; searchLength <span class="keyword">and</span> current[--index1] <span class="keyword">is</span> old[--index2]
+    <span class="keyword">while</span> count &lt; searchLength <span class="keyword">and</span> <span class="keyword">not</span> compare.basic current[--index1], old[--index2]
       count++
     count
   
@@ -515,11 +581,11 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         </li>
         
         
-        <li id="section-22">
+        <li id="section-26">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-22">&#182;</a>
+                <a class="pilcrow" href="#section-26">&#182;</a>
               </div>
               <p>“Deletion” columns</p>
 
@@ -532,11 +598,11 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         </li>
         
         
-        <li id="section-23">
+        <li id="section-27">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-23">&#182;</a>
+                <a class="pilcrow" href="#section-27">&#182;</a>
               </div>
               <p>“Addition” rows. Initialize null column.</p>
 
@@ -549,11 +615,11 @@ returned. Otherwise  <code>true</code> will be returned.</p>
         </li>
         
         
-        <li id="section-24">
+        <li id="section-28">
             <div class="annotation">
               
               <div class="pilwrap ">
-                <a class="pilcrow" href="#section-24">&#182;</a>
+                <a class="pilcrow" href="#section-28">&#182;</a>
               </div>
               <p>Initialize null row</p>
 
@@ -564,7 +630,7 @@ returned. Otherwise  <code>true</code> will be returned.</p>
     
     <span class="keyword">for</span> i <span class="keyword">in</span> [<span class="number">1.</span>..rowCount]
       <span class="keyword">for</span> j <span class="keyword">in</span> [<span class="number">1.</span>..columnCount]
-        <span class="keyword">if</span> current[currentStart + j - <span class="number">1</span>] <span class="keyword">is</span> old[oldStart + i - <span class="number">1</span>]
+        <span class="keyword">unless</span> compare.basic current[currentStart + j - <span class="number">1</span>], old[oldStart + i - <span class="number">1</span>]
           distances[i][j] = distances[i - <span class="number">1</span>][j - <span class="number">1</span>]
         <span class="keyword">else</span>
           north = distances[i - <span class="number">1</span>][j] + <span class="number">1</span>

src/compare.coffee
@@ -41,12 +41,29 @@ compare = {}
       oldValueValue = oldValue.valueOf()
       
       # Allow dates and Number/String objects to be compared
-      if valueValue isnt value and oldValueValue isnt oldValue
+      if typeof valueValue isnt 'object' and typeof oldValueValue isnt 'object'
         return valueValue isnt oldValueValue
       else
         changeRecords = compare.objects value, oldValue
         return if changeRecords.length then changeRecords else false
     # If a value has changed call the callback
+    else
+      return compare.basic value, oldValue
+  
+  
+  # Diffs two basic types, returning true if changed or false if not
+  compare.basic = (value, oldValue) ->
+   if value and oldValue and typeof value is 'object' and typeof oldValue is 'object'
+      # Allow dates and Number/String objects to be compared
+      valueValue = value.valueOf()
+      oldValueValue = oldValue.valueOf()
+      
+      # Allow dates and Number/String objects to be compared
+      if typeof valueValue isnt 'object' and typeof oldValueValue isnt 'object'
+        return compare.basic valueValue, oldValueValue
+    # If a value has changed call the callback
+    else if typeof value is 'number' and typeof oldValue is 'number' and isNaN(value) and isNaN(oldValue)
+      return false
     else
       return value isnt oldValue
   
@@ -65,21 +82,21 @@ compare = {}
   
     # Goes through the old object (should be a clone) and look for things that are now gone or changed
     for prop, oldValue of oldObject
-      newValue = object[prop]
+      value = object[prop]
       
       # Allow for the case of obj.prop = undefined (which is a new property, even if it is undefined)
-      continue if newValue isnt undefined and newValue is oldValue
+      continue if value isnt undefined and not compare.basic value, oldValue
           
       # If the property is gone it was removed
       unless `(prop in object)`
         changeRecords.push newChange object, 'deleted', prop, oldValue
         continue
       
-      if newValue isnt oldValue
+      if compare.basic value, oldValue
         changeRecords.push newChange object, 'updated', prop, oldValue
     
     # Goes through the old object and looks for things that are new
-    for prop, newValue of object
+    for prop, value of object
       continue if `prop in oldObject`
       
       changeRecords.push newChange object, 'new', prop
@@ -185,7 +202,7 @@ compare = {}
   # find the number of items at the beginning that are the same
   sharedPrefix = (current, old, searchLength) ->
     for i in [0...searchLength]
-      return i unless current[i] is old[i]
+      return i if compare.basic current[i], old[i]
     return searchLength
   
   
@@ -194,7 +211,7 @@ compare = {}
     index1 = current.length
     index2 = old.length
     count = 0
-    while count < searchLength and current[--index1] is old[--index2]
+    while count < searchLength and not compare.basic current[--index1], old[--index2]
       count++
     count
   
@@ -266,7 +283,7 @@ compare = {}
     
     for i in [1...rowCount]
       for j in [1...columnCount]
-        if current[currentStart + j - 1] is old[oldStart + i - 1]
+        unless compare.basic current[currentStart + j - 1], old[oldStart + i - 1]
           distances[i][j] = distances[i - 1][j - 1]
         else
           north = distances[i - 1][j] + 1

commit 90186a34c6ebbadbc20851aa466370a04ff9723f

Fixes critical comparison bug introduced with commit a8bbc80

dist/chip.js
@@ -2721,7 +2721,8 @@ if (!Date.prototype.toISOString) {
         if (typeof valueValue !== 'object' && typeof oldValueValue !== 'object') {
           return compare.basic(valueValue, oldValueValue);
         }
-      } else if (typeof value === 'number' && typeof oldValue === 'number' && isNaN(value) && isNaN(oldValue)) {
+      }
+      if (typeof value === 'number' && typeof oldValue === 'number' && isNaN(value) && isNaN(oldValue)) {
         return false;
       } else {
         return value !== oldValue;

docs/src/compare.html
@@ -238,7 +238,7 @@ returned. Otherwise  <code>true</code> will be returned.</p>
 
             </div>
             
-            <div class="content"><div class='highlight'><pre>    <span class="keyword">else</span> <span class="keyword">if</span> <span class="keyword">typeof</span> value <span class="keyword">is</span> <span class="string">'number'</span> <span class="keyword">and</span> <span class="keyword">typeof</span> oldValue <span class="keyword">is</span> <span class="string">'number'</span> <span class="keyword">and</span> isNaN(value) <span class="keyword">and</span> isNaN(oldValue)
+            <div class="content"><div class='highlight'><pre>    <span class="keyword">if</span> <span class="keyword">typeof</span> value <span class="keyword">is</span> <span class="string">'number'</span> <span class="keyword">and</span> <span class="keyword">typeof</span> oldValue <span class="keyword">is</span> <span class="string">'number'</span> <span class="keyword">and</span> isNaN(value) <span class="keyword">and</span> isNaN(oldValue)
       <span class="keyword">return</span> <span class="literal">false</span>
     <span class="keyword">else</span>
       <span class="keyword">return</span> value <span class="keyword">isnt</span> oldValue</pre></div></div>

src/compare.coffee
@@ -61,8 +61,9 @@ compare = {}
       # Allow dates and Number/String objects to be compared
       if typeof valueValue isnt 'object' and typeof oldValueValue isnt 'object'
         return compare.basic valueValue, oldValueValue
+    
     # If a value has changed call the callback
-    else if typeof value is 'number' and typeof oldValue is 'number' and isNaN(value) and isNaN(oldValue)
+    if typeof value is 'number' and typeof oldValue is 'number' and isNaN(value) and isNaN(oldValue)
       return false
     else
       return value isnt oldValue