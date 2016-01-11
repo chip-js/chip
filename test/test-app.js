1836dae19586ff6c84048057017a331c0766608c
Jacob Wright <jacob.wright@riotgames.com>
2016-01-11 08:07:36 -0700

    Adds routing and gets ready for testing

--- test/test-app.js
+++ test/test-app.js
@@ -1,25 +1,32 @@
 var chip = require('../index');
 
 describe('App', function() {
-  var app
+  var app;
 
   beforeEach(function() {
-    app = chip()
-  })
+    app = chip();
+  });
+
 
   it('should provide event functions', function() {
-    expect(app).to.have.a.property('dispatchEvent')
-    expect(app.dispatchEvent).to.be.a('function')
-    expect(app.on).to.be.a('function')
-    expect(app.one).to.be.a('function')
-    expect(app.off).to.be.a('function')
-  })
+    expect(app).to.have.a.property('dispatchEvent');
+    expect(app.dispatchEvent).to.be.a('function');
+    expect(app.on).to.be.a('function');
+    expect(app.one).to.be.a('function');
+    expect(app.off).to.be.a('function');
+  });
+
 
   it('should have a default root element of <html>', function() {
     expect(app).to.have.a.property('rootElement', document.documentElement)
-  })
+  });
 
-  it('should allow routing', function() {
-    expect(app).to.have.a.property('route')
-  })
-})
+
+  it('should create components', function() {
+    app.component('my-test', {
+      template: '<div id="my-test"></div>'
+    });
+
+    expect(app.fragments.binders.element['my-test']).to.not.be.null;
+  });
+});
