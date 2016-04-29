f494be6b4c9125ab4008634522545e5298d67052
Jacob Wright <jacwright@gmail.com>
2016-04-29 16:27:58 -0600

    Fix events to call within the correct scope when in repeats

--- dist/chip.js
+++ dist/chip.js
@@ -711,12 +711,21 @@ module.exports = function(ComponentClass) {
 };
 
 },{"./component-definition":14}],16:[function(require,module,exports){
+var functionCallExp = /(^|[^\.\]a-z$_\$])([a-z$_\$][a-z_\$0-9-]*)\s*\(\s*(\S)/ig;
+
 /**
  * A binder for adding event listeners. When the event is triggered the expression will be executed. The properties
  * `event` (the event object) and `element` (the element the binder is on) will be available to the expression.
  */
 module.exports = function(specificEventName) {
   return {
+    compiled: function() {
+      this.expression = this.expression.replace(functionCallExp, function(_, before, functionName, closingParen) {
+        var after = closingParen === ')' ? closingParen : ', ' + closingParen;
+        return before + functionName + '.call(_origContext_ || this' + after;
+      });
+    },
+
     created: function() {
       var eventName = specificEventName || this.match;
       var _this = this;
